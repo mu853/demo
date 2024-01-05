c520829ca2ecaa7c1c1b903e8065d9b0ce97d250
mu853 <mu.youichi853@gmail.com>
2024-01-05 19:16:56 +0000

    update

--- 82_set_policy_wc-1.sh
+++ 82_set_policy_wc-1.sh
@@ -5,7 +5,10 @@ kubectl config use-context wc-1-admin@wc-1
 kubectl apply -f 41_cg_red2_app1.yaml
 kubectl apply -f 42_cg_se_interface.yaml
 kubectl apply -f 43_cg_red1-web1.yaml
-kubectl apply -f 51_acnp_default_rules.yaml
-kubectl apply -f 52_acnp_inter_ns_red.yaml
-kubectl apply -f 53_acnp_db_red.yaml
-kubectl apply -f 54_acnp_se_red.yaml
+kubectl apply -f 44_cg_blue-app1.yaml
+kubectl apply -f 51_acnp_default_rules1.yaml
+kubectl apply -f 52_acnp_default_rules2.yaml
+kubectl apply -f 53_acnp_inter_ns_red.yaml
+kubectl apply -f 54_acnp_db_red.yaml
+kubectl apply -f 55_acnp_se_red.yaml
+kubectl apply -f 56_acnp_fluentbit.yaml
